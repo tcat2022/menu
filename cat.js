ae213b44ded824b82e5c949f7f982564b8310285
tommy <tcatty7@gmail.com>
2022-09-23 18:00:53 -0400

    f

---
+++ cat.js
@@ -0,0 +1,141 @@
+let canvas = document.getElementById("canvas")
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+ctx = canvas.getContext("2d")
+const gravity = 1.5
+class Player {
+    constructor(){
+        this.position = {
+            x:100,
+            y:100
+        }
+        this.velocity = {
+            x:0,
+            y:0
+        }
+        this.width = 30
+        this.height = 30
+    }
+    draw() {
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.position.x, this.position.y, 
+            this.width,this.height)
+
+    }
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y  
+        if(this.position.y + this.height + this.velocity.y
+            <= canvas.height
+            )
+                this.velocity.y += gravity
+                    else this.velocity.y = 0                 
+    }
+}
+class Platform {
+    constructor({x, y}){
+       this.position = {
+        x:x,
+        y:y
+       } 
+       this.width = 200
+       this.height = 20
+}
+draw() {
+    ctx.fillStyle = "blue"
+ctx.fillRect(this.position.x,this.position.y,
+     this.width, this.height)
+}
+}
+const player = new Player()
+const platforms = [new Platform({
+    x:400,
+    y:100
+}), new Platform({x: 300, y:300}),]
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
+
+
+function animate(){
+    requestAnimationFrame(animate)
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    player.update()
+    platforms.forEach((platform) => {
+        platform.draw()
+    })
+
+if(keys.right.pressed && player.position.x
+   < 400 ) {
+    player.velocity.x = 5
+} else if(keys.left.pressed && player.position.x
+    > 100){
+player.velocity.x = -5
+}else {player.velocity.x = 0
+
+    if(keys.right.pressed) { 
+        platforms.forEach(platform => {
+         platform.position.x -= 5 
+    }) 
+    }else if(keys.left.pressed) {
+        platforms.forEach(platform => {
+            platform.position.x += 5 
+        }) 
+     }
+} 
+platforms.forEach(platform => {
+ if(player.position.y + player.height <=
+  platform.position.y && player.position.y +
+  player.height + player.velocity.y >=
+  platform.position.y && player.position.x + 
+  player.width >= platform.position.x &&
+  player.position.x <= platform.position.x +
+   platform.width){
+player.velocity.y = 0
+ }
+})
+}
+animate()
+addEventListener('keydown', ({keyCode}) => {
+    switch(keyCode){
+   case 65:
+    console.log("left")
+    keys.left.pressed = true
+    break;
+    case 32:
+        player.velocity.y -= 20
+        console.log("up")
+     console.log("up")
+     break;
+    case 68:
+      console.log("right")
+      keys.right.pressed = true
+      break;
+     case 83:
+       console.log("down")
+}
+})
+addEventListener('keyup', ({keyCode}) => {
+    switch(keyCode){
+   case 65:
+    console.log("left")
+    keys.left.pressed = false
+    break;
+    case 32:
+        player.velocity.y -= 20
+     console.log("up")
+     break;
+    case 68:
+      console.log("right")
+      keys.right.pressed = false
+      break;
+     case 83:
+       console.log("down")
+}
+})
